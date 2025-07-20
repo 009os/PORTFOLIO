e0e55f87bcc4f51fdf26787fc584766e15e4b388
omji009 <omji@pi42.com>
2025-07-20 22:20:41 +0530

    Setup GitHub Pages deployment with custom domain portfolio009os

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: 'export',
+	trailingSlash: true,
+	basePath: process.env.NODE_ENV === 'production' ? '/PORTFOLIO' : '',
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 	experimental: {
 		mdxRs: true,
