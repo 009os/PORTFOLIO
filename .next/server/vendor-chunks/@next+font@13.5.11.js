a91d04357d356148025707bb57082a9e4c4aada1
omji009 <omji@pi42.com>
2025-07-20 21:18:01 +0530

    changes

---
+++ .next/server/vendor-chunks/@next+font@13.5.11.js
@@ -0,0 +1,34 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/@next+font@13.5.11";
+exports.ids = ["vendor-chunks/@next+font@13.5.11"];
+exports.modules = {
+
+/***/ "(rsc)/./node_modules/.pnpm/@next+font@13.5.11/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}":
+/*!*************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/.pnpm/@next+font@13.5.11/node_modules/@next/font/google/target.css?{"path":"app/layout.tsx","import":"Inter","arguments":[{"subsets":["latin"],"variable":"--font-inter"}],"variableName":"inter"} ***!
+  \*************************************************************************************************************************************************************************************************************************/
+/***/ ((module) => {
+
+eval("// Exports\nmodule.exports = {\n\t\"style\": {\"fontFamily\":\"'__Inter_e8ce0c', '__Inter_Fallback_e8ce0c'\",\"fontStyle\":\"normal\"},\n\t\"className\": \"__className_e8ce0c\",\n\t\"variable\": \"__variable_e8ce0c\"\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9ub2RlX21vZHVsZXMvLnBucG0vQG5leHQrZm9udEAxMy41LjExL25vZGVfbW9kdWxlcy9AbmV4dC9mb250L2dvb2dsZS90YXJnZXQuY3NzP3tcInBhdGhcIjpcImFwcC9sYXlvdXQudHN4XCIsXCJpbXBvcnRcIjpcIkludGVyXCIsXCJhcmd1bWVudHNcIjpbe1wic3Vic2V0c1wiOltcImxhdGluXCJdLFwidmFyaWFibGVcIjpcIi0tZm9udC1pbnRlclwifV0sXCJ2YXJpYWJsZU5hbWVcIjpcImludGVyXCJ9IiwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQSxXQUFXLGdGQUFnRjtBQUMzRjtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9jaHJvbmFyay5jb20vLi9ub2RlX21vZHVsZXMvLnBucG0vQG5leHQrZm9udEAxMy41LjExL25vZGVfbW9kdWxlcy9AbmV4dC9mb250L2dvb2dsZS90YXJnZXQuY3NzP2JiM2QiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gRXhwb3J0c1xubW9kdWxlLmV4cG9ydHMgPSB7XG5cdFwic3R5bGVcIjoge1wiZm9udEZhbWlseVwiOlwiJ19fSW50ZXJfZThjZTBjJywgJ19fSW50ZXJfRmFsbGJhY2tfZThjZTBjJ1wiLFwiZm9udFN0eWxlXCI6XCJub3JtYWxcIn0sXG5cdFwiY2xhc3NOYW1lXCI6IFwiX19jbGFzc05hbWVfZThjZTBjXCIsXG5cdFwidmFyaWFibGVcIjogXCJfX3ZhcmlhYmxlX2U4Y2UwY1wiXG59O1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(rsc)/./node_modules/.pnpm/@next+font@13.5.11/node_modules/@next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}\n");
+
+/***/ }),
+
+/***/ "(rsc)/./node_modules/.pnpm/@next+font@13.5.11/node_modules/@next/font/local/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"\",\"arguments\":[{\"src\":\"../public/fonts/CalSans-SemiBold.ttf\",\"variable\":\"--font-calsans\"}],\"variableName\":\"calSans\"}":
+/*!************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/.pnpm/@next+font@13.5.11/node_modules/@next/font/local/target.css?{"path":"app/layout.tsx","import":"","arguments":[{"src":"../public/fonts/CalSans-SemiBold.ttf","variable":"--font-calsans"}],"variableName":"calSans"} ***!
+  \************************************************************************************************************************************************************************************************************************************************/
+/***/ ((module) => {
+
+eval("// Exports\nmodule.exports = {\n\t\"style\": {\"fontFamily\":\"'__calSans_829c8b', '__calSans_Fallback_829c8b'\"},\n\t\"className\": \"__className_829c8b\",\n\t\"variable\": \"__variable_829c8b\"\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9ub2RlX21vZHVsZXMvLnBucG0vQG5leHQrZm9udEAxMy41LjExL25vZGVfbW9kdWxlcy9AbmV4dC9mb250L2xvY2FsL3RhcmdldC5jc3M/e1wicGF0aFwiOlwiYXBwL2xheW91dC50c3hcIixcImltcG9ydFwiOlwiXCIsXCJhcmd1bWVudHNcIjpbe1wic3JjXCI6XCIuLi9wdWJsaWMvZm9udHMvQ2FsU2Fucy1TZW1pQm9sZC50dGZcIixcInZhcmlhYmxlXCI6XCItLWZvbnQtY2Fsc2Fuc1wifV0sXCJ2YXJpYWJsZU5hbWVcIjpcImNhbFNhbnNcIn0iLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBLFdBQVcsK0RBQStEO0FBQzFFO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL2Nocm9uYXJrLmNvbS8uL25vZGVfbW9kdWxlcy8ucG5wbS9AbmV4dCtmb250QDEzLjUuMTEvbm9kZV9tb2R1bGVzL0BuZXh0L2ZvbnQvbG9jYWwvdGFyZ2V0LmNzcz85NjMwIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIEV4cG9ydHNcbm1vZHVsZS5leHBvcnRzID0ge1xuXHRcInN0eWxlXCI6IHtcImZvbnRGYW1pbHlcIjpcIidfX2NhbFNhbnNfODI5YzhiJywgJ19fY2FsU2Fuc19GYWxsYmFja184MjljOGInXCJ9LFxuXHRcImNsYXNzTmFtZVwiOiBcIl9fY2xhc3NOYW1lXzgyOWM4YlwiLFxuXHRcInZhcmlhYmxlXCI6IFwiX192YXJpYWJsZV84MjljOGJcIlxufTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(rsc)/./node_modules/.pnpm/@next+font@13.5.11/node_modules/@next/font/local/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"\",\"arguments\":[{\"src\":\"../public/fonts/CalSans-SemiBold.ttf\",\"variable\":\"--font-calsans\"}],\"variableName\":\"calSans\"}\n");
+
+/***/ })
+
+};
+;
